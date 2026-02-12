9c5b2145663e5cf8a2fc1733cb50e7d9804bd9eb
khristinatsymbala <khristinakorda@gmail.com>
2026-02-12 16:07:02 +0200

    Refactor code for better readability

--- Automation - Swag Labs /tests/Inventory.spec.ts
+++ Automation - Swag Labs /tests/Inventory.spec.ts
@@ -2,46 +2,73 @@ import { expect, test } from "./fixture/inventory.fixture";
 import { InventoryPage } from "../page-objects/InventoryPage";
 import { expectedLogoText } from "../test-data/inventoryPage/inventoryTestData"
 
+test('Check the correctness of the inventory page', async ({ onInventoryPage }) => {
+
+    console.log(onInventoryPage.checkOpen)
+    console.log('done')
+})
 
 test.describe('TS-05 - Check shopping functionality before redirecting to the shopping cart', () => {
 
-    test('Check the correctness of the inventory page', async ({ onInventoryPage }) => {
-
-        console.log(onInventoryPage.checkOpen)
-        console.log('done')
-    })
 
     test('TC_IP_01 - Verify the ability to add the product to the shopping cart', async ({ onInventoryPage }) => {
+
+        // Arrange
         const productIndex = 3
+
+        //Act
         await onInventoryPage.addProductToShoppingCartbyIndex(productIndex)
-        expect(await onInventoryPage.getShoppingCartBadgeCount()).toBe(await onInventoryPage.getAddedProductsCount());
+
+        // Assert
+        const cartBadgeCount = await onInventoryPage.getShoppingCartBadgeCount()
+        const addedProductsCount = await onInventoryPage.getAddedProductsCount()
+
+        expect(cartBadgeCount).toBe(addedProductsCount);
 
     })
     test('TC_IP_02 - Verify the ability to remove the product from the shopping cart', async ({ onInventoryPage }) => {
 
-        const productIndex = 1
-        await onInventoryPage.addProductToShoppingCartbyIndex(productIndex)
-        await onInventoryPage.addProductToShoppingCartbyIndex(3)
-        expect(await onInventoryPage.getAddedProductsCount()).toBe(2)
-        await onInventoryPage.removeProductToShoppingCartbyIndex(productIndex)
-        expect(await onInventoryPage.getAddedProductsCount()).toBe(1)
+        //Arrange
+        const firstProduct = 1
+        const secondProduct = 3
+
+        await onInventoryPage.addProductToShoppingCartbyIndex(firstProduct)
+        await onInventoryPage.addProductToShoppingCartbyIndex(secondProduct)
+
+        // Assert initial state
+        const initialCount = await onInventoryPage.getAddedProductsCount()
+        expect(initialCount).toBe(2)
+
+        //Act
+        await onInventoryPage.removeProductToShoppingCartbyIndex(firstProduct)
+
+        // Assert update state
+        const updateCount = await onInventoryPage.getAddedProductsCount()
+        expect(updateCount).toBe(1)
 
 
     })
     test('TC_IP_03 - Verify that the quantity icon (shopping_cart_badge) appears in the shopping cart when adding a product', async ({ onInventoryPage }) => {
-
+        //Arrange
         const productIndex = 3
+        //Act
         await onInventoryPage.addProductToShoppingCartbyIndex(productIndex)
+        //Assert
         await expect(onInventoryPage.shoppingCartBadge).toBeVisible();
 
     })
     test('TC_IP_04 - Verify the decrease in product quantity in the shopping_cart_badge after removing the product.', async ({ onInventoryPage }) => {
-
+        //Arrange
         const productIndexToRemove = 4
+
+        //Act
         onInventoryPage.addMultiplProductsToShoppingCart()
         await onInventoryPage.removeProductToShoppingCartbyIndex(productIndexToRemove)
 
-        expect(await onInventoryPage.getShoppingCartBadgeCount()).toBe(await onInventoryPage.getAddedProductsCount());
+        //Assert
+        const cartBadgeCount = await onInventoryPage.getShoppingCartBadgeCount()
+        const addedProductCount = await onInventoryPage.getAddedProductsCount()
+        expect(cartBadgeCount).toBe(addedProductCount);
     })
     test('TC_IP_05 - Verify the ability to add the same product twice to the shopping cart', async ({ onInventoryPage }) => {
         // 
